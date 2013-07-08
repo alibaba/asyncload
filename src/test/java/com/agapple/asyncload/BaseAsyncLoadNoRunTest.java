bc8c0792fd315d369cc519b90beda9531f9d333b
Linker Lin <linker.lin@me.com>
2013-07-08 11:28:59 +0800

    upgraded usage of JUnit

--- src/test/java/com/agapple/asyncload/BaseAsyncLoadNoRunTest.java
+++ src/test/java/com/agapple/asyncload/BaseAsyncLoadNoRunTest.java
@@ -2,16 +2,24 @@ package com.agapple.asyncload;
 
 import com.agapple.asyncload.impl.helper.AsyncLoadProxyRepository;
 import junit.framework.Assert;
-import org.junit.Test;
 import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit38.AbstractJUnit38SpringContextTests;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.concurrent.ConcurrentHashMap;
 
-@ContextConfiguration(locations = { "classpath:asyncload/applicationContext.xml" })
-public class BaseAsyncLoadNoRunTest extends AbstractJUnit38SpringContextTests {
-
+//@ContextConfiguration(locations = { "classpath:asyncload/applicationContext.xml" })
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {
+        "classpath*:/asyncload/application*.xml"})
+@Ignore
+public abstract class BaseAsyncLoadNoRunTest implements ApplicationContextAware {
+    public ApplicationContext applicationContext;
     @Before
     public void setUp() {
         // System.setProperty(DebuggingClassWriter.DEBUG_LOCATION_PROPERTY, "/tmp/cglib/");
@@ -23,9 +31,9 @@ public class BaseAsyncLoadNoRunTest extends AbstractJUnit38SpringContextTests {
         }
     }
 
-    @Test
-    public void testEmpty(){
-        Assert.assertTrue(true);
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException{
+        this.applicationContext = applicationContext;
     }
+
 
 }
